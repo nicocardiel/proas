e28491ea94ee12ca0424cdf84b93aa933df3f53c
Sergio Pascual <sergiopasra@users.noreply.github.com>
2009-09-20 22:48:49 +0000

    Removing all pgplot routines

--- src/dibuobje.f
+++ src/dibuobje.f
@@ -43,40 +43,38 @@ C
           Y=YMIN+(YMAX-YMIN)*ALTURA(J)/90
           X=XMIN+(XMAX-XMIN)*(TU(J)-TUINI)/(TUFIN-TUINI)
           IF(GRAF)THEN
-            CALL PGMOVE(X01,Y01)
-            CALL PGDRAW(X,Y)
+            CALL pljoin(X01,Y01,X,Y)
           ELSE
-            CALL PGPOINT(1,X,Y,-1)
+            CALL plpoin(1,X,Y,-1)
           END IF
           X01=X
           Y01=Y
-	  IF(OPCMEN.NE.5)THEN
+          IF(OPCMEN.NE.5)THEN
             IF(REAL(J+IALEAT)/NB.EQ.REAL((J+IALEAT)/NB))THEN
-              CALL PGSCH(0.4)
+CP              CALL PGSCH(0.4)
               WRITE(NUMERO,'(I2.2)')NBODY
 ccc              CALL PGPTEXT(X,Y+.5,0.,.5,NUMERO(2-NBODY/10:2))
-              CALL PGPTEXT(X,Y+.5,0.,.5,NUMERO)
+              CALL plptex(X,Y+.5,0.,.5,NUMERO)
             END IF
-	  END IF
+          END IF
 C
-	  IF(OPCMEN.EQ.1)THEN
+          IF(OPCMEN.EQ.1)THEN
             RADIO=RADG*(90-ALTURA(J))/90
             X=XCEN-RADIO*SIN(ACIMUT(J)*PI/180)
             Y=YCEN-RADIO*COS(ACIMUT(J)*PI/180)
             IF(GRAF)THEN
-              CALL PGMOVE(X02,Y02)
- 	      CALL PGDRAW(X,Y)
-	    ELSE
-              CALL PGPOINT(1,X,Y,-1)
-	    END IF
+              CALL pljoin(X02,Y02,X,Y)
+            ELSE
+              CALL plpoin(1,X,Y,-1)
+            END IF
             X02=X
             Y02=Y
             IF(REAL(J+IALEAT)/NB.EQ.REAL((J+IALEAT)/NB))THEN
-              CALL PGSCH(0.3)
+CP              CALL PGSCH(0.3)
               WRITE(NUMERO,'(I2.2)')NBODY
-              CALL PGPTEXT(X,Y+.5,0.,.5,NUMERO(2-NBODY/10:2))
+              CALL plptex(X,Y+.5,0.,.5,NUMERO(2-NBODY/10:2))
             END IF
-	  END IF
+          END IF
           GRAF=.TRUE.
         ELSE
           GRAF=.FALSE.
