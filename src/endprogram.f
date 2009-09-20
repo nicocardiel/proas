9cc86104c33b0fe151a99234a5335e3dd41138a5
Sergio Pascual <sergiopasra@users.noreply.github.com>
2009-09-20 21:59:09 +0000

    Removed all pgplot subroutines

--- src/endprogram.f
+++ src/endprogram.f
@@ -15,17 +15,17 @@ C CHARACTER*10 CADENA -> if CADENA='endprogram' STOP
 C
 Comment
 C------------------------------------------------------------------------------
-	SUBROUTINE ENDPROGRAM(CADENA)
-	IMPLICIT NONE
-	CHARACTER*10 CADENA
-	CHARACTER*1 CSTOP
-	IF(CADENA(1:10).EQ.'endprogram')THEN
-	  WRITE(*,100)'Do you want to STOP the program (y/n) [n] ?'
-	  READ(*,'(A)') CSTOP
-	  IF(CSTOP.EQ.'y')THEN
-	    CALL PGEND
-	    STOP
-	  END IF
-	END IF
-100	FORMAT(A,$)
-	END
+        SUBROUTINE ENDPROGRAM(CADENA)
+        IMPLICIT NONE
+        CHARACTER*10 CADENA
+        CHARACTER*1 CSTOP
+        IF(CADENA(1:10).EQ.'endprogram')THEN
+          WRITE(*,100)'Do you want to STOP the program (y/n) [n] ?'
+          READ(*,'(A)') CSTOP
+          IF(CSTOP.EQ.'y')THEN
+            CALL PLEND
+            STOP
+          END IF
+        END IF
+100     FORMAT(A,$)
+        END
